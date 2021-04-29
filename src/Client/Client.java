c375a54faf87a6b7ca024156762e2a986514ec59
zara.alessio <zara.alessio@TAVE-PC34.OSDBSESTO.local>
2021-04-29 14:04:56 +0200

    Esercizio Tecnologie

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pongesort;
+package Client;
 
 /**
  *
  * @author zara.alessio
  */
-public class PongeSort {
+public class Client {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
+
---
+++ src/Server/PongeSort.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+/**
+ *
+ * @author zara.alessio
+ */
+public class PongeSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char [] arr = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'q', 'l'};
+    }
+    
+}
